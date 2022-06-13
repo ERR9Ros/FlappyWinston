d17bc05bbdd9b392f7e5ec617643bcf35f3dece4
ERR9Ros <88700442+ERR9Ros@users.noreply.github.com>
2022-06-13 14:44:37 +1000

    QoL work and player Functions

--- sketch.js
+++ sketch.js
@@ -1,20 +1,21 @@
-let pipe = [340, 240, 120, 400, 540, 440, 320, 600];
-let Pheight = [0, 0, 0, 0, 0, 0, 0, 0];
-
-let birdY = 180;
+let wall = [340, 240, 120, 400, 540, 440, 320, 600];
+let wallHeight = [0, 0, 0, 0, 0, 0, 0, 0];
+let player = [];
 
 let lost = false;
 let speed = 60;
 
-highScore = getItem("highScore");
-if (highScore == null) {
-  highScore = 0;
-}
-score = 0;
-
 function setup() {
   var cnv = createCanvas(windowWidth, windowHeight);
   cnv.position(0, 0);
+
+  player.push(new bird());
+
+  highScore = getItem("highScore");
+  if (highScore == null) {
+    highScore = 0;
+  }
+  score = 0;
 }
 
 function windowResized() {
@@ -24,5 +25,17 @@ function windowResized() {
 
 function draw() {
   background(0);
+  fill(255);
 
+  //Walls
+  //Player
+  for (let p of player) {
+    p.display();
+    p.move();
+  }
+}
+function keypPressed() {
+  for (let p of player) {
+    p.jump();
+  }
 }
